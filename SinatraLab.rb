ac91ee0ae71729baab7dc8175cc86561f1fa1abb
Eishaank Gakhar <egakhar1@andrew.cmu.edu>
2019-02-01 20:46:07 +0000

    Added response.erb

--- SinatraLab.rb
+++ SinatraLab.rb
@@ -2,5 +2,16 @@ require 'sinatra'
 
 get '/' do
     erb :index
+end
+
+post '/' do
+    "Hello World"
+    @fname = params[:firstname]
+    @lname = params[:lastname]
+    @class_love = params[:class_love]
+    @feedback = params[:feedback]
     
+    @happy = (@class_love == "More than I care to say") ? "That's Great!" : "Well, hopefullyyou'll REALLY love it by the end"
+    
+    erb :response
 end
