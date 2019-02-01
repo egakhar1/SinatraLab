8afbcf078947035c6664dcb7ce0e03c83d1a7be0
Eishaank Gakhar <egakhar1@andrew.cmu.edu>
2019-02-01 23:50:04 +0000

    added application.erb and simplified other erb files

--- SinatraLab.rb
+++ SinatraLab.rb
@@ -1,17 +1,20 @@
 require 'sinatra'
 
 get '/' do
-    erb :index
+    erb :application do
+        erb :index
+    end
 end
 
 post '/' do
-    "Hello World"
     @fname = params[:firstname]
     @lname = params[:lastname]
     @class_love = params[:class_love]
     @feedback = params[:feedback]
     
-    @happy = (@class_love == "More than I care to say") ? "That's Great!" : "Well, hopefullyyou'll REALLY love it by the end"
+    @happy = (@class_love == "More than I care to say") ? "That's Great!" : "Well, hopefully you'll REALLY love it by the end"
     
-    erb :response
+    erb :application do
+        erb :response
+    end
 end
